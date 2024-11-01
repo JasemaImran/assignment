a682391364675ade445e2f1b3c2814508339ae0e
JasemaImran <155939454+JasemaImran@users.noreply.github.com>
2024-11-01 10:24:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cal_age age1 = new cal_age();
+            Console.WriteLine(age1.calculate_age());
+            Console.ReadLine(); 
+
+        }
+    }
+}
